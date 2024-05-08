c3f36b578efe04433cc4ad0dbe418ea0bd34a3c2
Jeffrey Lee <jefflee8807@gmail.com>
2024-05-07 23:56:31 -0700

    Explore Progress

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,42 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+test('matches valid phone numbers', () => {
+  expect(isPhoneNumber('(012)345-6789')).toBe(true);
+});
+test('matches valid phone numbers', () => {
+  expect(isPhoneNumber('(987)654-3210')).toBe(true);
+});
+test('matches valid phone numbers', () => {
+  expect(isPhoneNumber('0123456789')).toBe(false);
+});
+test('matches valid phone numbers', () => {
+  expect(isPhoneNumber('9876543210')).toBe(false);
+});
+
+
+test('matches valid emails', () => {
+  expect(isEmail('123@gmail.com')).toBe(true);
+});
+test('matches valid emails', () => {
+  expect(isEmail('abc@ucsd.edu')).toBe(true);
+});
+test('matches valid emails', () => {
+  expect(isEmail('@gmail')).toBe(false);
+});
+test('matches valid emails', () => {
+  expect(isEmail('ucsd.edu')).toBe(false);
+});
+
+test('matches valid emails', () => {
+  expect(isEmail('123@gmail.com')).toBe(true);
+});
+test('matches valid emails', () => {
+  expect(isEmail('abc@ucsd.edu')).toBe(true);
+});
+test('matches valid emails', () => {
+  expect(isEmail('@gmail')).toBe(false);
+});
+test('matches valid emails', () => {
+  expect(isEmail('ucsd.edu')).toBe(false);
+});
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,69 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+function init()
+{
+  const faceImg = document.querySelector('header + img');
+  const textArea = document.getElementById('text-to-speak');
+  const voiceSelection = document.getElementById('voice-select');
+  const button = document.querySelector('button');
+
+  const synth = window.speechSynthesis;
+  let voices = [];
+  let text = '';
+  let selectedVoice;
+
+  if( synth.onvoiceschanged !== undefined )
+    synth.onvoiceschanged = function() {voices = populateVoiceList(synth, voiceSelection)};
+
+  textArea.addEventListener('input', (event) => {
+    text = event.target.value;
+  });
+  voiceSelection.addEventListener('change', function(event) {
+    const selected = event.target.options[event.target.selectedIndex];
+    for(const voice of voices) {
+      if(voice.name === selected.getAttribute('data-name')) {
+        selectedVoice = voice;
+        break;
+      }
+    }
+  });
+  button.addEventListener('click', (event) => talk(event, synth, text, selectedVoice, faceImg));
+}
+
+function populateVoiceList(synth, select)
+{
+  const voices = synth.getVoices();
+
+  for(const voice of voices ) {
+    const option = document.createElement('option');
+    const name = voice.name;
+    const lang = voice.lang;
+    option.textContent = `${name} (${lang})`;
+    if(voice.default)
+      option.textContent += " -- DEFAULT";
+    option.setAttribute('data-lang', lang );
+    option.setAttribute('data-name', name );
+    select.appendChild(option);
+  }
+
+  return voices;
+}
+function talk(event, synth, text, voice, img)
+{
+  const utterance = new SpeechSynthesisUtterance(text);
+  const initImgSrc = img.src;
+  const initImgAlt = img.alt;
+
+  utterance.voice = voice;
+  utterance.onstart = () => {
+    img.src = 'assets/images/smiling-open.png';
+    img.alt = 'Open smiling face';
+  };
+  utterance.onend = () => {
+    img.src = initImgSrc;
+    img.alt = initImgAlt;
+  };
+  synth.speak(utterance);
+
 }
