8b0c81bd6b67e0608750f6b6047a956c70399177
Jeffrey Lee <jefflee8807@gmail.com>
2024-05-08 00:35:57 -0700

    explore finish unittests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -35,15 +35,41 @@ test('matches valid emails', () => {
   expect(isEmail('ucsd.edu')).toBe(false);
 });
 
-test('matches valid emails', () => {
-  expect(isEmail('123@gmail.com')).toBe(true);
+test("The password's first character must be a letter, it must contain at least * 4 characters and no more than 15 characters and no characters other than * * letters, numbers and the underscore may be used", () => {
+  expect(isStrongPassword('hello_world')).toBe(true);
 });
-test('matches valid emails', () => {
-  expect(isEmail('abc@ucsd.edu')).toBe(true);
+test("The password's first character must be a letter, it must contain at least * 4 characters and no more than 15 characters and no characters other than * * letters, numbers and the underscore may be used", () => {
+  expect(isStrongPassword('abcdef789')).toBe(true);
 });
-test('matches valid emails', () => {
-  expect(isEmail('@gmail')).toBe(false);
+test("The password's first character must be a letter, it must contain at least * 4 characters and no more than 15 characters and no characters other than * * letters, numbers and the underscore may be used", () => {
+  expect(isStrongPassword('~123456789123456789')).toBe(false);
 });
-test('matches valid emails', () => {
-  expect(isEmail('ucsd.edu')).toBe(false);
+test("The password's first character must be a letter, it must contain at least * 4 characters and no more than 15 characters and no characters other than * * letters, numbers and the underscore may be used", () => {
+  expect(isStrongPassword('')).toBe(false);
+});
+
+test("This regular expressions matches dates of the form XX / XX / YYYY where XX can be 1 or 2 digits long and YYYY is always 4 digits long.", () => {
+  expect(isDate('3/14/2015')).toBe(true);
+});
+test("This regular expressions matches dates of the form XX / XX / YYYY where XX can be 1 or 2 digits long and YYYY is always 4 digits long.", () => {
+  expect(isDate('10/7/2024')).toBe(true);
+});
+test("This regular expressions matches dates of the form XX / XX / YYYY where XX can be 1 or 2 digits long and YYYY is always 4 digits long.", () => {
+  expect(isDate('5/7/4')).toBe(false);
+});
+test("This regular expressions matches dates of the form XX / XX / YYYY where XX can be 1 or 2 digits long and YYYY is always 4 digits long.", () => {
+  expect(isDate('6789/0/2000')).toBe(false);
+});
+
+test("Matches valid 3 or 6 character hex codes used for HTML or CSS.", () => {
+  expect(isHexColor('#fff')).toBe(true);
+});
+test("Matches valid 3 or 6 character hex codes used for HTML or CSS.", () => {
+  expect(isHexColor('#000000')).toBe(true);
+});
+test("Matches valid 3 or 6 character hex codes used for HTML or CSS.", () => {
+  expect(isHexColor('#fffffff')).toBe(false);
+});
+test("Matches valid 3 or 6 character hex codes used for HTML or CSS.", () => {
+  expect(isHexColor('#0')).toBe(false);
 });
